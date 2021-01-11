c070ae627785f20e30d279f6adc49a400ecafc80
kamileecher <58888200+kamileecher@users.noreply.github.com>
2021-01-11 21:40:16 +0300

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -3,14 +3,14 @@ import json
 from distutils.util import strtobool as stb
 
 # --------------------------------------
-BOT_TOKEN = ""
-GDRIVE_FOLDER_ID = ""
+BOT_TOKEN = "1570761081:AAFFnTROpNWo6NPrgpc7tiaDyPs100Uqx2A"
+GDRIVE_FOLDER_ID = "1BjpZ04did3X3VS8eKCZA9nhe9mmi2Skb"
 # Default folder id.
-OWNER_ID = 123455673
+OWNER_ID = 558204588
 # Example: OWNER_ID = 619418070
-AUTHORISED_USERS = []
+AUTHORISED_USERS = [558204588]
 # Example: AUTHORISED_USERS = [63055333, 100483029, -1003943959]
-INDEX_URL = ""
+INDEX_URL = "postgres://zluhocpmbqhlnm:f758b938e8b8cf8e68b67b45f4ceafb9e8656a815e62a53b45032ec9dd21ee57@ec2-54-147-98-183.compute-1.amazonaws.com:5432/df382ublqbklsp"
 IS_TEAM_DRIVE = True
 USE_SERVICE_ACCOUNTS = True
 # --------------------------------------
